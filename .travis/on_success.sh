6fdb7351a11412ffaaedeb438504ee04bd204e4f
Loïc <loic.dumas@c-s.fr>
2018-07-30 13:10:20 +0200

    replace github token by git ssh key to CS-SI/SIRIUS and make sure only CS-SI/SIRIUS will use it

--- .travis/on_success.sh
+++ .travis/on_success.sh
@@ -1,4 +1,9 @@
-if [ -n "${GITHUB_TOKEN}" ]; then
+if [ "x${TRAVIS_REPO_SLUG}" = "xCS-SI/SIRIUS" ]; then
+    openssl aes-256-cbc -K $encrypted_f05cf190aee2_key -iv $encrypted_f05cf190aee2_iv -in github_deploy_key.enc -out github_deploy_key -d
+    chmod 600 deploy-key
+    eval `ssh-agent -s`
+    ssh-add deploy-key
+
     cd "$TRAVIS_BUILD_DIR"
     mkdir doc/sphinx/build
     docker run -v $TRAVIS_BUILD_DIR/doc/sphinx:/sphinx ldumas/sirius_dockerfile:sirius_pages /bin/sh -c "cd /sphinx; make html"
@@ -8,8 +13,7 @@ if [ -n "${GITHUB_TOKEN}" ]; then
     git checkout -b gh-pages
     git add .
     git -c user.name='travis' -c user.email='travis' commit -m init
-    git push -q -f https://dumasl:${GITHUB_TOKEN}@github.com/dumasl/SIRIUS gh-pages &2>/dev/null
-    #git push -f https://dumasl:${GITHUB_TOKEN}@github.com/dumasl/SIRIUS gh-pages
+    git push -q -f gh-pages &2>/dev/null
     cd "$TRAVIS_BUILD_DIR"
 fi
 
