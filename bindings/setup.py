198760cf07fe2bb6be15ed884b402bdad5b41818
Rashad Kanavath <rashad.kanavath@c-s.fr>
2018-09-04 14:46:34 +0200

    use setuputils.Extension and define enable_logs macro

--- bindings/setup.py
+++ bindings/setup.py
@@ -1,63 +1,29 @@
-from setuptools import setup, Extension
+import os, sysconfig
+from setuptools import setup, Extension, distutils
 from setuptools.command.build_ext import build_ext
-import sys
-import setuptools
+from setuptools.dist import Distribution
+
+class BinaryDistribution(Distribution):
+    def is_pure(self):
+        return False
+
+extra_compile_args = sysconfig.get_config_var('CFLAGS').split()
+extra_compile_args.append('-std=c++14')
+#TODO: use generated config.h and avoid SIRIUS_ENABLE_LOGS and others
+extra_compile_args.append('-DSIRIUS_ENABLE_LOGS=1')
+
+# extra_compile_args.append(cpp_flag(self.compiler))
+# if has_flag(self.compiler, '-fvisibility=hidden'):
+#     opts.append('-fvisibility=hidden')
 
 ext_modules = [
-    Extension('siriuspy',
+    Extension(name='siriuspy',
         sources=['siriuspy.cpp'],
         libraries=['libsirius'],
+        extra_compile_args=extra_compile_args,
         language='c++'
     ),
 ]
-
-# As of Python 3.6, CCompiler has a `has_flag` method.
-# cf http://bugs.python.org/issue26689
-def has_flag(compiler, flagname):
-    """Return a boolean indicating whether a flag name is supported on
-    the specified compiler.
-    """
-    import tempfile
-    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
-        f.write('int main (int argc, char **argv) { return 0; }')
-        try:
-            compiler.compile([f.name], extra_postargs=[flagname])
-        except setuptools.distutils.errors.CompileError:
-            return False
-    return True
-
-
-def cpp_flag(compiler):
-    """Return the -std=c++14 compiler flag. this is required minimum for sirius"""
-    if has_flag(compiler, '-std=c++14'):
-        return '-std=c++14'
-    else:
-        raise RuntimeError('Unsupported compiler -- at least C++11 support '
-                           'is needed!')
-
-class BuildExt(build_ext):
-    """A custom build extension for adding compiler-specific options."""
-    c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': [],
-    }
-
-    if sys.platform == 'darwin':
-        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-
-    def build_extensions(self):
-        ct = self.compiler.compiler_type
-        opts = self.c_opts.get(ct, [])
-        if ct == 'unix':
-            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
-            opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, '-fvisibility=hidden'):
-                opts.append('-fvisibility=hidden')
-        elif ct == 'msvc':
-            opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
-        for ext in self.extensions:
-            ext.extra_compile_args = opts
-        build_ext.build_extensions(self)
 
 setup(
     name='siriuspy',
@@ -68,6 +34,6 @@ setup(
     long_description='Fast and simple to plug-in C++ resampling library that is taking advantage of the Fourier Transform',
     ext_modules=ext_modules,
     #install_requires=['pybind11>=2.2'],
-    cmdclass={'build_ext': BuildExt},
-    zip_safe=False,
+    #cmdclass={'build_ext': Extension},
+    zip_safe=False
 )
