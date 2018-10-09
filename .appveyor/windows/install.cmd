0c2955f93ed5d8c9a6b150b261c04497d7a45d13
Herve Ly-Sunnaram <herve.ly-sunnaram@c-s.fr>
2018-10-09 09:33:02 +0200

    appveyor: update dependencies installation

--- .appveyor/windows/install.cmd
+++ .appveyor/windows/install.cmd
@@ -6,10 +6,15 @@ appveyor AddMessage "Updating vcpkg knowledge base..."
 :: update vcpkg knowledge base
 ::   git pull
 
-:: use fork to fix multiple issues with release only version of the dependencies
+:: TODO: remove after merges of vcpkg PR #4413 (geos) and #4433 (gdal)
+:: use fork to fix multiple build issues with release only version of the dependencies
+git config --global user.email "test@test.com"
+git config --global user.name "Test Test"
 git remote add release-build https://github.com/hlysunnaram/vcpkg.git || goto error
-git fetch release-build fix-vcpck-build-type || goto error
-git checkout fix-vcpck-build-type || goto error
+git fetch release-build update/gdal-2.3.2 || goto error
+git fetch release-build update/geos-3.6.3 || goto error
+git checkout update/gdal-2.3.2 || goto error
+git merge release-build/update/geos-3.6.3 || goto error
 
 :: need to execute bootstrapping in a new process since it exits the current cmd process at the end of its execution...
 cmd.exe /C "bootstrap-vcpkg.bat -disableMetrics" || goto error
@@ -26,7 +31,7 @@ vcpkg install fftw3:%VCPKG_TRIPLET% || goto error
 appveyor AddMessage "Installing FFTW3 dependency done"
 
 appveyor AddMessage "Installing GDAL dependency..."
-vcpkg install gdal:%VCPKG_TRIPLET% || goto error
+vcpkg install gdal[core,mysql-libmariadb]:%VCPKG_TRIPLET% || goto error
 appveyor AddMessage "Installing GDAL dependency done"
 
 exit /b 0
