5c56ad793ec6db212ad68f4534c656b3ba756115
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2020-03-10 08:43:16 +0100

    Modif APPVEYOR - windows build - remove vcpkg install sqlite. Only proj4 before gdal

--- .appveyor/windows/install.cmd
+++ .appveyor/windows/install.cmd
@@ -20,11 +20,8 @@ appveyor AddMessage "Installing FFTW3 dependency..."
 vcpkg install fftw3:%VCPKG_TRIPLET% || goto error
 appveyor AddMessage "Installing FFTW3 dependency done"
 
-appveyor AddMessage "Installing SQLITE dependency..."
-vcpkg install sqlite:%VCPKG_TRIPLET% || goto error
-
-appveyor AddMessage "Installing PROJ dependency..."
-vcpkg install proj:%VCPKG_TRIPLET% || goto error
+appveyor AddMessage "Installing PROJ4 dependency..."
+vcpkg install proj4:%VCPKG_TRIPLET% || goto error
 
 appveyor AddMessage "Installing GDAL dependency..."
 vcpkg install gdal[core,mysql-libmariadb]:%VCPKG_TRIPLET% || goto error
