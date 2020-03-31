0955c602a8f0dce10fb8e463ae319ade68d26698
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2020-03-31 18:11:16 +0200

    Update branch fix_ctests_data with develop

--- tests/utils.h
+++ tests/utils.h
@@ -30,14 +30,6 @@
 namespace sirius {
 namespace tests {
 
-// filter paths
-static constexpr char kDiracFilterPath[] = "./filters/dirac_filter.tif";
-static constexpr char kSincZoom2FilterPath[] =
-      "./filters/sinc_zoom2_filter.tif";
-
-// image paths
-static constexpr char kLenaImagePath[] = "./input/lena.jpg";
-
 sirius::Image CreateDummyImage(const sirius::Size& size);
 
 sirius::Image CreateSquaredImage(const sirius::Size& size);
