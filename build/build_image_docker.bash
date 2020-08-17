f80afe92b1cc07f63b8fee9f013f387613a27635
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2020-08-17 14:42:53 +0200

    Improve Docker image

--- build/build_image_docker.bash
+++ build/build_image_docker.bash
@@ -10,7 +10,6 @@ LOCAL_PROJECT_DIR=$1
 LOCAL_PROJECT_DIR="$(readlink -f ${LOCAL_PROJECT_DIR})"
 
 CURRENT_DIR=$(pwd)
-DOCKER_IMAGENAME="sirius"
 # TODO: SIRIUS_VERSION ?
 cd ${LOCAL_PROJECT_DIR}
 SIRIUS_GIT_BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
@@ -18,7 +17,7 @@ SIRIUS_GIT_BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
 DOCKER_VAR_SIRIUS_REVISION_COMMIT="sirius_revision_commit"
 
 #DOCKER_VAR_SIRIUS_VERSION=${SIRIUS_GIT_BRANCH_NAME}
-DOCKER_VAR_SIRIUS_VERSION="0.2.0"
+DOCKER_VAR_SIRIUS_VERSION="0.3.0"
 
 echo " "
 echo " * PROJECT_DIR=${LOCAL_PROJECT_DIR}"
@@ -28,16 +27,19 @@ DOCKER_VAR_INSTALL_DIR="/opt/sirius"
 DOCKER_VAR_BUILD_DIR="/tmp/sirius_build_dir"
 DOCKER_VAR_CMAKE_BUILD_TYPE=Release
 
-echo " * DOCKER_IMAGENAME=${DOCKER_IMAGENAME}"
 echo " * DOCKER_VAR_SIRIUS_VERSION=${DOCKER_VAR_SIRIUS_VERSION}"
 echo " * DOCKER_VAR_SIRIUS_REVISION_COMMIT=${DOCKER_VAR_SIRIUS_REVISION_COMMIT}"
 echo " * DOCKER_VAR_INSTALL_DIR=${DOCKER_VAR_INSTALL_DIR}"
 echo " * DOCKER_VAR_BUILD_DIR=${DOCKER_VAR_BUILD_DIR}"
 
+# Build sirius docker base
+DOCKER_IMAGENAME="sirius.base"
+echo " * DOCKER_IMAGENAME=${DOCKER_IMAGENAME}"
+
 # Build docker
 # ----------------------
 sudo docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg no_proxy=$no_proxy  -t ${DOCKER_IMAGENAME}:latest  -t ${DOCKER_IMAGENAME}:${DOCKER_VAR_SIRIUS_VERSION} -f- . <<EOF
-FROM ubuntu:18.04
+FROM osgeo/gdal:latest
 LABEL maintainer="CS SI"
 
 ENV DEBIAN_FRONTEND noninteractive
@@ -65,14 +67,15 @@ RUN apt-get update -y \\
     python3-pip \\
     python3-dev \\
     python3-numpy \\
-    python3-gdal \\
-    libgdal-dev \\
-    gdal-bin \\
     git
+#    python3-gdal \\
+#    libgdal-dev \\
+#    gdal-bin \\
 # && apt-get autoclean -y \
 # && apt-get autoremove -y \
 # && rm -rf /var/lib/apt/lists/*
 
+RUN gdalinfo --version
 
 WORKDIR ${DOCKER_VAR_BUILD_DIR}
 COPY . ${DOCKER_VAR_BUILD_DIR}
@@ -91,8 +94,27 @@ ENV PATH=${DOCKER_VAR_INSTALL_DIR}/bin:\$PATH
 
 EOF
 
-# Save docker image
+# Build sirius docker OPER
+DOCKER_IMAGENAME_OPER="sirius"
+echo " * DOCKER_IMAGENAME_OPER=${DOCKER_IMAGENAME_OPER}"
+
+# Build docker
 # ----------------------
+sudo docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg no_proxy=$no_proxy  -t ${DOCKER_IMAGENAME_OPER}:latest  -t ${DOCKER_IMAGENAME_OPER}:${DOCKER_VAR_SIRIUS_VERSION} -f- . <<EOF
+FROM ${DOCKER_IMAGENAME}:${DOCKER_VAR_SIRIUS_VERSION}
+LABEL maintainer="CS SI"
+
+ENTRYPOINT ["${DOCKER_VAR_INSTALL_DIR}/bin/sirius"]
+
+EOF
+
 cd ${CURRENT_DIR}
+# Save docker image base
+# ----------------------
 echo "Save the image docker ${DOCKER_IMAGENAME}_${DOCKER_VAR_SIRIUS_VERSION}.tar..." \
   && sudo docker save ${DOCKER_IMAGENAME}:${DOCKER_VAR_SIRIUS_VERSION} -o ${DOCKER_IMAGENAME}_${DOCKER_VAR_SIRIUS_VERSION}.tar
+
+# Save docker image
+# ----------------------
+echo "Save the image docker ${DOCKER_IMAGENAME_OPER}_${DOCKER_VAR_SIRIUS_VERSION}.tar..." \
+  && sudo docker save ${DOCKER_IMAGENAME_OPER}:${DOCKER_VAR_SIRIUS_VERSION} -o ${DOCKER_IMAGENAME_OPER}_${DOCKER_VAR_SIRIUS_VERSION}.tar
